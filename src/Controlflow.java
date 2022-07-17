1b56529f9e87885917389a0dffe63371c6eeec3e
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2022-07-17 20:57:31 +0100

    added forloop

--- src/Controlflow.java
+++ src/Controlflow.java
@@ -6,11 +6,13 @@ public class Controlflow {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         
-        if (number % 5 == 0)
-            System.out.println("Fizz");
         if (number % 5 == 0 && number % 3 == 0)
             System.out.println("FizzBuzz");
-        else if(number % 5 != 0 && number % 3 != 0)
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else
             System.out.println(number);
     }
 }
